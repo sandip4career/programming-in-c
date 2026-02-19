a3686f6d3e7a86bb0aae123ebfa58d9749d53446
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-02-19 16:40:51 +0530

    Add files via upload

---
+++ find-output-3.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int x=5;
+
+    //assignment operator instead of eqaul-to operator used....
+    if(x=4)
+    {
+        printf("value is %d",x);
+    }
+    else
+    {
+        printf("value is %d",x);
+    }
+
+    getch();
+    return 0;
+}
