b3774498ec27e31284e823396e594b236377016c
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-02-25 12:27:10 +0530

    Add files via upload

---
+++ even-odd-modulus.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number:");
+    scanf("%d",&num);
+
+    if(num%2)
+    {
+        printf("%d is odd",num);
+    }
+    else
+    {
+        printf("%d is even",num);
+    }
+    return 0;
+}
