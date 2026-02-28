d2f2d76a3989eb2d62f349e100109725111b0a61
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-02-28 11:12:27 +0530

    Add files via upload

---
+++ first-N-natural-number.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,N;
+
+    printf("Enter value of N:");
+    scanf("%d",&N);
+
+    for(i=1;i<=N;i++)
+    {
+        printf("%d ",i);
+    }
+
+    return 0;
+}
