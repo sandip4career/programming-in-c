d7252e2b25b80027e4e58de4c54d55d7ba18bd54
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-03-07 13:59:12 +0530

    Add files via upload

---
+++ reverse-a-number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int num,r,rev=0;
+
+    printf("Enter a number:");
+    scanf("%d",&num);
+
+    while(num!=0)
+    {
+        r=num%10;
+        rev= rev*10+r;
+        num=num/10;
+    }
+
+    printf("reverse is %d",rev);
+    return 0;
+}
