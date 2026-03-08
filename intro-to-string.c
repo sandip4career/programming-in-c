df30ca0546297b81edb786fa15673655e3205750
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-03-08 10:55:14 +0530

    Add files via upload

--- intro-to-string.c
+++ intro-to-string.c
@@ -2,8 +2,19 @@
 
 int main()
 {
-    char s[20] = {'s','a','n','d','i','p','\0'};
-    int i;
+    //types of declaration
+    //char s[20] = {'s','a','n','d','i','p','\0'};
+    //char s[20] = "Sun";
+    char s[20];
+
+    //taking input from user
+    printf("Enter your name:");
+    //scanf("%s",&s[0]);
+
+    //talking multiword string multiword string
+    gets(s);
+
+   /* int i;
     //print-first technique - 1-1 character
     i=0;
     while(s[i]!='\0')
@@ -11,13 +22,13 @@ int main()
         printf("%c",s[i]);
         i++;
     }
-
+    */
 
     //2nd technique direct
     //printf("%s",s);
 
     //3rd technique
-    //puts(s);
+    puts(&s[0]);
 
     return 0;
 }
