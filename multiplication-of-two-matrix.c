32a747b5fbad3f970bcc9738626f81c05695f671
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-03-12 20:54:46 +0530

    Add files via upload

---
+++ multiplication-of-two-matrix.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+
+int main()
+{
+    int A[3][3], B[3][3], C[3][3], i, j, k, sum;
+
+    printf("Enter 9 numbers for first matrix:\n");
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+        {
+            scanf("%d",&A[i][j]);
+        }
+        //printf("\n");
+
+    }
+
+    printf("Enter 9 numbers for second matrix:\n");
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+        {
+            scanf("%d",&B[i][j]);
+        }
+            //printf("\n");
+    }
+
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+        {
+            sum = 0;
+
+            for(k=0;k<=2;k++)
+            {
+                sum += A[i][k] * B[k][j];
+            }
+
+            C[i][j] = sum;
+        }
+    }
+
+    printf("Result matrix:\n");
+
+    for(i=0;i<=2;i++)
+    {
+        for(j=0;j<=2;j++)
+            printf("%d ",C[i][j]);
+
+        printf("\n");
+    }
+}
