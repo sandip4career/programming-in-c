7cbb85a465b9441dbfc393e10f81cecd6d13f069
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-03-16 15:55:11 +0530

    Add files via upload

--- appication-of-pointer-2.c
+++ appication-of-pointer-2.c
@@ -1,20 +1,29 @@
 #include<stdio.h>
+void input(int *);
+void display(int *);
 
 int main()
 {
-    int arr[5],*p,i;
-    p=&arr[0];
+    int a[5];
+    input(a);
+    display(a);
+    return 0;
+}
 
+void input(int *p)
+{
+    int i;
     for(i=0;i<=4;i++)
     {
         scanf("%d",p+i);
     }
+}
 
+void display(int *p)
+{
+    int i;
     for(i=0;i<=4;i++)
     {
         printf("%d ",*(p+i));
     }
-
-
-    return 0;
 }
