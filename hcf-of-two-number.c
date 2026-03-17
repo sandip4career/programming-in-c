cef959a19e1e32498e95e925a196032e4721e3d3
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-03-17 16:13:29 +0530

    Add files via upload

--- hcf-of-two-number.c
+++ hcf-of-two-number.c
@@ -3,11 +3,18 @@
 
 int main()
 {
-    int a,b,hcf;
+    int a,b,h;
     printf("Enter two number:");
     scanf("%d %d",&a,&b);
 
 
-    printf("HCF is %d",,hcf);
+    for(h=a<b?a:b;h>=1;h--)
+    {
+        if(a%h==0 && b%h==0)
+        {
+            break;
+        }
+    }
+    printf("HCF is %d",h);
     return 0;
 }
