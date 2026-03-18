1d9d450247502115e8fa27a8c816d2c9077c0619
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-03-18 16:09:48 +0530

    Add files via upload

--- application-of-pointers-3.c
+++ application-of-pointers-3.c
@@ -1,20 +1,35 @@
 #include<stdio.h>
 
-void swap(int*,int*);
+int length(char*);
+char* reverse(char*);
+
 int main()
 {
-    int x,y;
+    char s[12] = "computer";
 
-    printf("Enter two numbers:");
-    scanf("%d %d",&x,&y);
-    swap(&x,&y);
-    printf("x=%d y=%d",x,y);
-    return 0;
+    printf("%d", length(s));
+    printf("\n%s", reverse(s));
 }
 
-void swap(int* a,int *b)
+int length(char *p)
 {
-    int t = *a;
-    *a=*b;
-    *b=t;
+    int i;
+    for(i = 0; *(p + i) != '\0'; i++);
+    return i;
+}
+
+char* reverse(char *p)
+{
+    int l, i, temp;
+
+    for(l = 0; *(p + l) != '\0'; l++);
+
+    for(i = 0; i < l / 2; i++)
+    {
+        temp = *(p + i);
+        *(p + i) = *(p + l - 1 - i);
+        *(p + l - 1 - i) = temp;
+    }
+
+    return p;
 }
