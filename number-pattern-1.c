6d9f4a83c942cfbbe085aec1d7a3cb48a8ecf4de
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-03-19 19:26:05 +0530

    Add files via upload

--- number-pattern-1.c
+++ number-pattern-1.c
@@ -4,14 +4,15 @@ int main()
 {
     int i,j,k;
 
-    for(i=1;i<=5;i++)
+    for(i=1;i<=4;i++)
     {
         k=1;
-        for(j=1;j<=9;j++)
+        for(j=1;j<=7;j++)
         {
             if(j>=5-i&&j<=3+i)
             {
                 printf("%d",k);
+                (j<4)?k++:k--;
             }
             else
             {
