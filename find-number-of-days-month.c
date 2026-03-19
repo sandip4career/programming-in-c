e40677b1af1353de7d8ebe824a9dbefcc77a7337
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-03-19 15:14:15 +0530

    Add files via upload

---
+++ find-number-of-days-month.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+{
+    int m;
+    printf("Enter month:");
+    scanf("%d",&m);
+
+    switch(m)
+    {
+
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        printf("31 days");
+        break;
+    case 2:
+        printf("28 days");
+        break;
+
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        printf("30 days");
+        break;
+
+    default:
+        printf("Invalid month number");
+    }
+
+    return 0;
+}
