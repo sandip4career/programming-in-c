adb278c3df849b5492d2cfd70774a03c7e649d46
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-04-11 12:18:52 +0530

    Add files via upload

---
+++ writing-content-in-a-file.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+    int i;
+    FILE *fp;
+    char s[15] = "Hello world";
+    fp =fopen("f1.txt","w");
+
+    if(fp==NULL)
+    {
+        printf("file cannot open");
+        exit(1);
+    }
+
+    for(i=0;i<strlen(s);i++)
+    {
+        fputc(s[i],fp);
+    }
+            fclose(fp);
+        getch();
+
+        return 0;
+
+}
