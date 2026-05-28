bcb31abf70a329cb8e74b6474e586328531f12a4
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-05-28 20:47:03 +0530

    Add files via upload

---
+++ co-prime-number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,min;
+    int i;
+    printf("Enter two numbers:");
+    scanf("%d %d",&a,&b);
+    min = a<b?a:b;
+
+    for(i=2;i<=min;i++)
+    {
+        if(a%i==0&&b%i==0)
+    {
+        break;
+    }
+    }
+
+
+    if(i==min+1)
+    {
+        printf("%d and %d co-prime",a,b);
+    }
+    else
+    {
+        printf("%d and %d are not co-prime",a,b);
+    }
+
+    return 0;
+}
