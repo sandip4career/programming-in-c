d307261633f6f55988d0080ccdc661491560b32a
Sandip Yadav <131138946+sandip4career@users.noreply.github.com>
2026-06-01 21:48:01 +0530

    Add files via upload

--- star-pattern-5.c
+++ star-pattern-5.c
@@ -3,13 +3,14 @@
 int main()
 {
     int i,j;
+
     for(i=1;i<=5;i++)
     {
         for(j=1;j<=9;j++)
         {
             if(j>=6-i&&j<=4+i)
             {
-              printf("*");
+                printf("*");
             }
             else
             {
